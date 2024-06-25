00f890bfa70581d8e79a5f5bae95008867a8c886
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2024-06-25 22:14:31 +0530

    Add files via upload

--- Array.java
+++ Array.java
@@ -1,5 +1,27 @@
 import java.util.*;
-public class Array{
+ public class Array{
+//     public static int linearsearch(int marks[],int key){
+//         for(int i=0;i<marks.length;i++){
+//             if(marks[i]==key){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+//     public static int getLargest(int marks[]){
+//         int lar=Integer.MIN_VALUE;
+//         int small=Integer.MAX_VALUE;
+//         for(int i=0;i<marks.length;i++){
+//             if(lar<marks[i]){
+//                 lar=marks[i];
+//             }
+//             if(small>marks[i]){
+//                 small=marks[i];
+//             }
+//         }
+//         System.out.println("smallest: "+small);
+//         return lar;
+    
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in) ;
             System.out.println("The size of the array is :");
@@ -12,5 +34,27 @@ public class Array{
                for(int i=0;i<marks.length;i++){
                 // marks[i]=sc.nextInt();
                 System.out.print(marks[i]+ " ");}
+                System.out.println(" ");
+                // System.out.println("Enter the key you want to search: ");
+                // int key=sc.nextInt();
+                // int ls =linearsearch(marks,key);
+                // if(ls==-1){
+                //     System.out.println("Key Not found.");
+                // }
+                // else{
+                //     System.out.println("Key found at index : " +ls);
+                // }
+            //    int larmark=getLargest(marks);
+            //    System.out.println("Largest : "+larmark);
+            for(int i= marks.length-1;i<=0;i--){
+                int j=0;
+                marks[j]=marks[i];
+                j++;
+            }
+                for(int i=0;i<marks.length;i++){
+                System.out.print(marks[i]+ " ");
+                }
+                System.out.println(" ");
+
         }
     }
